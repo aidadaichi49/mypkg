b95d2fa97a1e1f08e58daa451a8469c4f4d0c895
Daichi Aida <da1649ichi@gmail.com>
2025-01-05 00:52:42 +0900

    Add a test

--- test/test.bash
+++ test/test.bash
@@ -2,4 +2,15 @@
 # SPDX-FileCopyrightText: 2025 Daichi Aida <da1649ichi@gmail.com>
 # SPDX-License-Identifier: BSD-3-Clause
 
+dir=~
+[ "$1" != "" ] && dir="$1"
 
+cd $dir/ros2_ws
+
+colcon build
+
+source $dir/.bashrc
+
+echo "hello" | timeout 10 ros2 run mypkg keyboard_talker > output.txt 2>&1
+
+grep 'hello' output.txt
